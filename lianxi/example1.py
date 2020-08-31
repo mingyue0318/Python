1ab3fe59909dd93e77cfeb2f57c069805ed6ba89
yao <xiaoyu_wu@foxitsoftware.cn>
2020-08-31 16:30:09 +0800

    fix

---
+++ lianxi/example1.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+'''
+Descripttion: 
+version: 
+Author: sueRimn
+Date: 2020-08-31 11:31:14
+LastEditors: sueRimn
+LastEditTime: 2020-08-31 16:17:53
+'''
+
+print('世界')
+
+
+def aaa():
+    print('!!!!!!!!!!!1');
+
+
+    def bbb():
+            print(3333333333)
