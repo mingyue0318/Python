7413c809c7d3074ff0739e4e5a880b5b7c9fbf42
yao <xiaoyu_wu@foxitsoftware.cn>
2020-09-01 18:46:03 +0800

    fix

--- lianxi/example1.py
+++ lianxi/example1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: UTF-8 -*-
 '''
 Descripttion: 
@@ -5,5 +6,27 @@ version:
 Author: sueRimn
 Date: 2020-08-31 11:31:14
 LastEditors: sueRimn
-LastEditTime: 2020-08-31 17:55:41
+LastEditTime: 2020-09-01 17:55:23
 '''
+raw_input("按下 enter 键退出，其他任意键显示...\n")
+
+a, b, c = 1, 2, "john"
+
+print a
+print b
+print c
+
+str = 'abcdefg'
+
+print str[1:5]
+
+print str[1:5:3]
+
+list = ['runoob', 786, 2.32, 'john', 70.2]
+
+var = 1
+while var == 1:
+    num = raw_input('Enter a number :')
+    print "You entered:", num
+    print(1*3)
+print "GOOD BYE"
