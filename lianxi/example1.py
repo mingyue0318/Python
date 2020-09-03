848e0c927e6255fc7c3403f0a38da27ab187a8f8
yao <xiaoyu_wu@foxitsoftware.cn>
2020-09-03 16:50:24 +0800

    fix

--- lianxi/example1.py
+++ lianxi/example1.py
@@ -6,27 +6,27 @@ version:
 Author: sueRimn
 Date: 2020-08-31 11:31:14
 LastEditors: sueRimn
-LastEditTime: 2020-09-01 17:55:23
+LastEditTime: 2020-09-03 10:16:50
 '''
-raw_input("按下 enter 键退出，其他任意键显示...\n")
+input("按下 enter 键退出，其他任意键显示...\n")
 
 a, b, c = 1, 2, "john"
 
-print a
-print b
-print c
+print(a)
+print (b)
+print (c)
 
 str = 'abcdefg'
 
-print str[1:5]
+print (str[1:5])
 
-print str[1:5:3]
+print (str[1:5:3])
 
 list = ['runoob', 786, 2.32, 'john', 70.2]
 
 var = 1
 while var == 1:
-    num = raw_input('Enter a number :')
-    print "You entered:", num
-    print(1*3)
-print "GOOD BYE"
+    num = input('Enter a number :')
+    print ("You entered:", num)
+    print(1 * 3)
+print ("GOOD BYE")
