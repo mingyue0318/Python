c046f9634098baf05498a2e9b5d674a4d8fd21f2
yao <xiaoyu_wu@foxitsoftware.cn>
2020-09-30 14:45:30 +0800

    Revert "fix"
    This reverts commit 8d256642d2856150c2b9685d79eee69aa382819d.

--- lianxi/using_name.py
+++ lianxi/using_name.py
@@ -4,11 +4,9 @@ version:
 Author: sueRimn
 Date: 2020-09-03 18:40:36
 LastEditors: sueRimn
-LastEditTime: 2020-09-14 11:34:04
+LastEditTime: 2020-09-03 18:41:37
 '''
 if __name__ == '__main__':
     print('程序自身执行')
 else:
     print('调用')
-
-
